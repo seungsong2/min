a784b3f3b7df8bd24d9fbcef7a304ac5078348ec
seungsong2 <s6257@naver.com>
2024-08-13 17:09:13 +0900

    t

---
+++ pythonProject1/5.py
@@ -0,0 +1,27 @@
+
+From 2pm to 3 pm
+At 'kimpo' Airport
+In Room 35
+
+
+it will be held on at
+No actually  [it] will start at 시간
+No actually  [it] will finish at 시간
+
+There are [] sessions
+First at 시간 there is a workshop on 주제  by 발표자
+Next at 시간 there is a Discussion on 주제 by 발표자
+
+
+You will depart from  At 시간 on 비행편
+No actually, [there was supposed to be an meeting[ [with ~] [at 시간] [On 요일] [but it has been canceled].
+
+No actually you will have a dinner meeting With 누구 The 직책, at 시간, So, it's not possible
+
+Bachelor's degree
+Master's degree
+She got a Master's degree in 분야 from 학교 in 년도
+
+First , from 2015 to 2017 She worked at 어디서 as 직책명 in 년도
+
+from 2015 up to now
